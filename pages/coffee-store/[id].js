903c7b30ae921579e66fde98ade6c5b7fb292042
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-04-24 00:53:21 +0530

    SSG static paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,37 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import Link from 'next/link';
+import React from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import CoffeeStoreData from "../../data/coffee-stores.json";
 
-const CoffeeStore = () => {
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: CoffeeStoreData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id;
+      }),
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }, { params: { id: "300" } }],
+    fallback: false,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
   // console.log(router);
   return (
     <div>
       Coffee Store Page {router.query.id}
-      <Link href='/'>Back to Home</Link>
-      <Link href='/coffee-store/dynamic'>Go to Dynamic page</Link>
+      <Link href="/">Back to Home</Link>
+      <Link href="/coffee-store/dynamic">Go to Dynamic page</Link>
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
     </div>
-  )
-}
+  );
+};
 
-export default CoffeeStore
+export default CoffeeStore;
